3ace067d3dceb865325fffaa0950b808f1dc8d90
ChandlerFan12138 <524592395@qq.com>
2022-08-26 21:51:52 +0100

    Feign 最佳实践--将Feign做成一个模块，注意：Bean问题

--- cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -1,14 +1,17 @@
 package cn.itcast.order;
 
+
+
+import cn.itcast.feign.clients.UserClient;
+import cn.itcast.feign.config.FeignClientConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @MapperScan("cn.itcast.order.mapper")
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients(defaultConfiguration = FeignClientConfiguration.class,clients = {UserClient.class})
 public class OrderApplication {
 
     public static void main(String[] args) {
--- cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ cloud-demo/order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,9 +1,12 @@
 package cn.itcast.order.service;
 
-import cn.itcast.order.clients.UserClient;
+
+import cn.itcast.feign.pojo.User;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
-import cn.itcast.order.pojo.User;
+
+import cn.itcast.feign.clients.UserClient;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +21,7 @@ public class OrderService {
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        User user = userClient.findByID(orderId);
+        User user =  userClient.findByID(orderId);
         order.setUser(user);
         // 4.返回
         return order;
