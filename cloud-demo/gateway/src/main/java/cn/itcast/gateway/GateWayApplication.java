b4d65bc2da0314daaa79c3d278af0a34bca3281a
ChandlerFan12138 <524592395@qq.com>
2022-08-26 22:26:33 +0100

    Gateway：网关路由工作已完成

---
+++ cloud-demo/gateway/src/main/java/cn/itcast/gateway/GateWayApplication.java
@@ -0,0 +1,11 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class, args);
+    }
+}
